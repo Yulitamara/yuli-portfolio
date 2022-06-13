88fa43b7e0ff81d4d6067b0640974cfa34d2280d
Yulitamara <79260463+Yulitamara@users.noreply.github.com>
2022-06-13 18:05:46 +0300

    deploy my react app to github pages

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,27 +7,27 @@ import { BsWhatsapp } from "react-icons/bs";
 // import emailjs from "@emailjs-com";
 
 const Contact = () => {
-  const form = useRef();
+  // const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "YOUR_SERVICE_ID",
-        "YOUR_TEMPLATE_ID",
-        form.current,
-        "YOUR_PUBLIC_KEY"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+  //   emailjs
+  //     .sendForm(
+  //       "YOUR_SERVICE_ID",
+  //       "YOUR_TEMPLATE_ID",
+  //       form.current,
+  //       "YOUR_PUBLIC_KEY"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  // };
 
   return (
     <section id="contact">
