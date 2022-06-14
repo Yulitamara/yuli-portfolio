b24742d27a975e14b4870503f4986021d92611e1
Yulitamara <79260463+Yulitamara@users.noreply.github.com>
2022-06-14 12:00:52 +0300

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,9 @@ import { IoLogoTwitter } from "react-icons/io";
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo"></a>
+      <a href="#" className="footer__logo">
+        Yuli Tamara
+      </a>
 
       <ul className="permalinks">
         <li>
@@ -43,6 +45,10 @@ const Footer = () => {
         <a href="https://twitter.com">
           <IoLogoTwitter />
         </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Yuli Tamara. All rights reserved.</small>
       </div>
     </footer>
   );
